b0f8bc4737e0503ff5e0f4d25bb061e13627cafb
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-07-29 15:45:36 -0300

    Time: 124 ms (5.96%), Space: 68.7 MB (5.52%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.ts
@@ -0,0 +1,27 @@
+function merge(intervals: number[][]): number[][] {
+    const concatenatedIntervals = []
+
+    const sortedIntervals = intervals.sort((intA, intB) => intA[0] - intB[0])
+    let currentInterval = sortedIntervals[0]
+
+
+    for (let i = 0; i < intervals.length; i++) {
+        if (i + 1 < sortedIntervals.length) {
+            console.log(sortedIntervals[i][1], sortedIntervals[i + 1][0])
+        }
+
+        if (
+            i + 1 < sortedIntervals.length &&
+            currentInterval[1] >= sortedIntervals[i + 1][0]
+        ) {
+            const [start, end] = currentInterval;
+            currentInterval = [Math.min(start, sortedIntervals[i + 1][0]), Math.max(sortedIntervals[i + 1][1], end)]
+
+        } else {
+            concatenatedIntervals.push(currentInterval)
+            currentInterval = sortedIntervals[i + 1]
+        }
+    }
+
+    return concatenatedIntervals
+};
