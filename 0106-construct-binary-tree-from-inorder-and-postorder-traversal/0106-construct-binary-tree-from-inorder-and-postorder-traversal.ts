adbddaca15adb58413c15bf1d90a29c662c40b0c
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-08-07 11:06:27 -0300

    Time: 331 ms (5.13%), Space: 137.2 MB (5.13%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.ts
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
+    if(!inorder.length){
+        return null;
+    }
+
+    const rootVal = postorder.pop()
+    const inOrderRootIndex = inorder.indexOf(rootVal)
+    const root = new TreeNode(rootVal)
+
+    const leftInorderSlice = inorder.slice(0, inOrderRootIndex)
+    const rightInorderSlice = inorder.slice(inOrderRootIndex + 1)
+
+    const leftPostOrderSlice = postorder.slice(0, leftInorderSlice.length)
+    const rightPostOrderSlice = postorder.slice(leftInorderSlice.length)
+
+    console.log('leftInorderSlice: ', leftInorderSlice)
+    console.log('rightInorderSlice: ', rightInorderSlice)
+
+    console.log('leftPostOrderSlice: ', leftPostOrderSlice)
+    console.log('rightPostOrderSlice: ', rightPostOrderSlice)
+
+    root.left = buildTree(leftInorderSlice, leftPostOrderSlice)
+    root.right = buildTree(rightInorderSlice, rightPostOrderSlice)
+
+    return root;
+};
