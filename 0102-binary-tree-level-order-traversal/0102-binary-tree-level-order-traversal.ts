fd40a085b3d3c4fc7c4cef04b612a9b90883c151
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-08-12 09:10:29 -0300

    Time: 5 ms (11.12%), Space: 61.4 MB (10.14%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrder(root: TreeNode | null): number[][] {
+    if (!root) {
+        return [];
+    }
+
+    const stack: [TreeNode, number][] = [[root, 0]];
+    const levelsMap = new Map<number, number[]>()
+
+    while (stack.length) {
+        const [node, level] = stack.pop()
+        const currentLevelNodeValues = levelsMap.get(level) ?? []
+        levelsMap.set(level, [...currentLevelNodeValues, node.val])
+
+        if (node.right) {
+            stack.push([node.right, level + 1])
+        }
+
+        if (node.left) {
+            stack.push([node.left, level + 1])
+        }
+
+    }
+
+    return Array.from(levelsMap.values())
+};
