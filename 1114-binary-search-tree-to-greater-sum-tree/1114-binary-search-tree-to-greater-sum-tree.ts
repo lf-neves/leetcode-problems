c70e093c09df6d7cd132fa24ec197d2b9a9074c2
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-09-30 15:34:03 -0300

    Time: 0 ms (100%), Space: 54.7 MB (80.26%) - LeetHub

---
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+
+
+function bstToGst(root: TreeNode | null): TreeNode | null {
+    var sum=0;
+    function recursion(root: TreeNode){
+        if(root == null)
+            return;
+        
+        recursion(root.right);
+        sum+=root.val;
+        root.val=sum;
+        recursion(root.left);
+    };
+    
+    recursion(root);
+
+    return root;
+};
