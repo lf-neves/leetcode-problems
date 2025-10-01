7e1813ceaa97133502ee454129cc209d123b2219
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-10-01 06:20:05 -0300

    Time: 60 ms (69.83%), Space: 65.5 MB (86.44%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(
+  root: TreeNode | null,
+  p: TreeNode,
+  q: TreeNode
+): TreeNode | null {
+  if (!root || root === p || root === q) {
+    return root;
+  }
+
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+
+  if (left && right) {
+    // p and q are in different subtrees -> root is LCA
+    return root;
+  }
+  // Otherwise, either both are in left subtree or both in right (or none)
+  return left !== null ? left : right;
+}
