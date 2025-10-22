8d7a59b84a42f175d21df4199e3398c11edaad27
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-10-22 10:59:17 -0300

    Time: 1 ms (76.67%), Space: 65.5 MB (66.67%) - LeetHub

---
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function convertBST(root: TreeNode | null): TreeNode | null {
+
+    let currentSum = 0
+
+    const traverse = (node: TreeNode) => {
+        if (!node) {
+            return;
+        }
+
+
+        traverse(node.right)
+        currentSum += node.val;
+        node.val = currentSum;
+        traverse(node.left)
+    }
+
+    traverse(root)
+
+    return root;
+};
