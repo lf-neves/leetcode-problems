441a7e02ccdb1c6de78ae3b5456fdbb170433bd3
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-10-22 10:21:38 -0300

    Time: 2 ms (70.55%), Space: 59 MB (76.55%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+    if(!nums.length){
+        return null;
+    }
+
+    const rootIndex = Math.floor(nums.length/2)
+    const root = new TreeNode(nums[rootIndex])
+    
+    root.left = sortedArrayToBST(nums.slice(0, rootIndex))
+    root.right = sortedArrayToBST(nums.slice(rootIndex + 1))
+    
+    return root;
+    
+};
