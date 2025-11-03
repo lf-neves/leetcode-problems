8159ea540a4dba3294bdf7f98e800d220a0f5b9c
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-11-03 11:53:06 -0300

    Time: 0 ms (100%), Space: 55.3 MB (65.6%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+    const result = []
+
+    const dfs = (node: TreeNode | null) => {
+        if(!node){
+            return;
+        }
+
+        dfs(node.left)
+        result.push(node.val)
+        dfs(node.right)
+
+        return;
+    }
+
+    dfs(root)
+
+    return result;
+};
