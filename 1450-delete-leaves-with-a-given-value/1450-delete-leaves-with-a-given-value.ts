92902c94c66c907147b2f11914bb161b4311cd84
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-11-03 10:58:35 -0300

    Time: 1 ms (67.86%), Space: 62.5 MB (28.57%) - LeetHub

---
+++ 1450-delete-leaves-with-a-given-value/1450-delete-leaves-with-a-given-value.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function removeLeafNodes(root: TreeNode | null, target: number): TreeNode | null {
+    const dfs = (node: TreeNode | null) => {
+        if (!node) {
+            return null;
+        }
+
+        node.left = dfs(node.left)
+        node.right = dfs(node.right)
+
+        if (!node.left && !node.right && node.val === target) {
+            return null
+        }
+
+        return node;
+    }
+
+    return dfs(root)
+
+};
