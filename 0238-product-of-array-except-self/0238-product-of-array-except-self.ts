f1f25df7e3b1e119b7f8c1cde1b5083d315bc05c
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-11-05 11:44:46 -0300

    Time: 16 ms (24.52%), Space: 72.7 MB (24.71%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,17 +1,24 @@
 function productExceptSelf(nums: number[]): number[] {
-    const result: number[] = new Array(nums.length).fill(1)
+    const n = nums.length;
+    const result = new Array(n);
+    const prefix = new Array(n);
+    const suffix = new Array(n);
 
-    let product = 1
-    for (let i = 0; i < nums.length; i++) {
-        result[i] *= product
-        product *= nums[i]
+    prefix[0] = nums[0];
+    for(let i = 1; i < n; i++){
+        prefix[i] = prefix[i-1] * nums[i];
     }
 
-    product = 1
-    for (let i = (nums.length - 1); i >= 0; i--) {
-        result[i] *= product
-        product *= nums[i]
+    suffix[n-1] = nums[n-1];
+    for(let i = n-2; i >= 0; i--){
+        suffix[i] = suffix[i+1] * nums[i];
     }
 
-    return result
+    for(let i = 0; i < n; i++){
+        const left = prefix[i-1] ?? 1;
+        const right = suffix[i+1] ?? 1;
+        result[i] = left * right;
+    }
+
+    return result;
 };
