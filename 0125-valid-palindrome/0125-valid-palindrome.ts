7ac20d5d8b0fd672e94280dc3ec233334e949bad
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-11-07 11:25:45 -0300

    Time: 333 ms (5.07%), Space: 67.5 MB (5.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,16 +1,33 @@
-function isPalindrome(s: string): boolean {    
-    const newStr = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+const isNumber = (s: string) => s.charCodeAt(0) >= 48 && s.charCodeAt(0) <= 57
+const isLetter = (s: string) => s.charCodeAt(0) >= 97 && s.charCodeAt(0) <= 122
 
-    if(newStr.length === 0){
+function isPalindrome(s: string): boolean {    
+    if(s.length === 1){
         return true
     }
 
-    let start = 0;
-    let end = newStr.length -1 
+    const lowerCaseString = s.toLowerCase()
+
+    let newStr = ''
+    let start = 0
+    let end = s.length - 1
 
     while(start < end){
-        if(newStr[start] !== newStr[end]){
-            console.log('joined')
+        console.log("start: ", start)
+        console.log("end: ", end)
+
+        if(!isNumber(lowerCaseString[start]) && !isLetter(lowerCaseString[start])){ 
+            start++;
+            continue;
+        }
+
+        if(!isNumber(lowerCaseString[end]) && !isLetter(lowerCaseString[end])){
+            end--;
+            continue
+        }
+
+
+        if(lowerCaseString[start] !== lowerCaseString[end]){
             return false
         }
 
