1c6bbe5846f23b402bf86a2c2d1215a38daa0047
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-11-11 09:57:12 -0300

    Time: 0 ms (100%), Space: 58.4 MB (80.94%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -1,14 +1,11 @@
 function removeDuplicates(nums: number[]): number {
-    const map = {}
-
-    for(let i=0;i<nums.length;i++){
-        if(map[nums[i]]){
-            nums.splice(i, 1)
-            i--;
-        } else {
-            map[nums[i]] = 1
+    let index = 1
+    for(let i=1;i<nums.length;i++){
+        if(nums[i-1] !== nums[i]){
+            nums[index] = nums[i]
+            index++
         }
     }
 
-    return nums.length
+    return index;
 };
