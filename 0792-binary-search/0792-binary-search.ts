2982da3827c56b6d049267aa9ae898eda8abc5ed
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-11-12 09:13:34 -0300

    Time: 3 ms (5.56%), Space: 59.1 MB (8.77%) - LeetHub

--- 0792-binary-search/0792-binary-search.ts
+++ 0792-binary-search/0792-binary-search.ts
@@ -1,3 +1,21 @@
 function search(nums: number[], target: number): number {
-    return nums.indexOf(target)
+    let i = 0, j = nums.length - 1
+
+    while (i <= j) {
+        const k = Math.floor((i + j) / 2)
+
+        console.log(i, j, k)
+
+        if(nums[k] === target){
+            return k;
+        }
+
+        if(nums[k] < target){
+            i = k + 1
+        } else {
+            j = k - 1
+        }
+    }
+
+    return -1
 };
