1338c00d1e7597c34e627425204cb877ae9d986e
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-11-13 08:59:49 -0300

    Time: 0 ms (100%), Space: 55.2 MB (61.46%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.ts
@@ -0,0 +1,15 @@
+function findMin(nums: number[]): number {
+    let start = 0, end = nums.length - 1
+
+    while (start < end) {
+        const mid = Math.floor((start + end) / 2)
+
+        if (nums[mid] > nums[end]) {
+            start = mid + 1
+        } else {
+            end = mid
+        }
+    }
+
+    return nums[start];
+};
