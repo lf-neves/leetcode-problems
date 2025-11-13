935dd454e85bd915d2c7bb5e0900f32010b19a76
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-11-13 09:33:47 -0300

    Time: 7 ms (16.65%), Space: 58.8 MB (22.38%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.ts
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -1,21 +1,22 @@
 function isValid(s: string): boolean {
-   const stack = []
-   const map = {
-    '(':')',
-    '[':']',
-    '{':'}'
-   }  
+    const stack = []
+    const map = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    }
 
-   for(let i=0;i<s.length;i++){
-        if(s[i] === '(' || s[i] === '[' || s[i] === '{'){
+    for (let i = 0; i < s.length; i++) {
+        if (Object.keys(map).includes(s[i])) {
             stack.push(s[i])
-        }else {
-            const lastOpened = stack.pop()
-            if(map[lastOpened] !== s[i]){
-                return false;
+        } else {
+            const lastAddedOp = stack.pop()
+
+            if (map[lastAddedOp] !== s[i]) { 
+                return false
             }
         }
-   }
+    }
 
-   return stack.length === 0;
+    return stack.length === 0
 };
