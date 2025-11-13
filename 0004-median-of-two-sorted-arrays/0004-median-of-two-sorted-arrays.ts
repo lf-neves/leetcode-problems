758d6b7ad16d20795fe4a3737bdae8b55f727fbf
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-11-13 09:23:43 -0300

    Time: 62 ms (8.8%), Space: 65.3 MB (7.15%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
@@ -1,12 +1,12 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-    nums1.push(...nums2);
-    const sortedArr = nums1.sort((a, b) => a-b)
+    const sortedArr = [...nums1, ...nums2].sort((a, b) => a - b)
 
     console.log(sortedArr)
 
-    if(sortedArr.length % 2 === 0){
-        return (sortedArr[(sortedArr.length/2)-1] + sortedArr[(sortedArr.length/2)])/2
+    if(sortedArr.length % 2 !== 0){
+        const mid = Math.floor(sortedArr.length/2)
+        return sortedArr[mid]
     }
 
-    return sortedArr[Math.floor(sortedArr.length/2)];
+    return (sortedArr[(sortedArr.length/2)-1] + sortedArr[(sortedArr.length/2)])/2
 };
