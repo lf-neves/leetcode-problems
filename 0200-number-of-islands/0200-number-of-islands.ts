0ba9c533a2e9ea539564f7fa3d8aeaf4bee565a0
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-11-25 10:50:35 -0300

    Time: 51 ms (94.73%), Space: 59.2 MB (72.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.ts
+++ 0200-number-of-islands/0200-number-of-islands.ts
@@ -1,28 +1,27 @@
 function numIslands(grid: string[][]): number {
     const removeLand = (x: number, y: number) => {
-        if (x < 0 || y < 0 || x >= grid[0].length || y >= grid.length || grid[y][x] !== '1') {
+        if(x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] !== '1'){
             return;
         }
 
-        grid[y][x] = '0';
+        grid[x][y] = "0"
 
-        removeLand(x - 1, y);
-        removeLand(x + 1, y);
-        removeLand(x, y - 1);
-        removeLand(x, y + 1);
+        removeLand(x, y+1)
+        removeLand(x, y-1)
+        removeLand(x+1, y)
+        removeLand(x-1, y)
     }
+    
+    let numOfIslands = 0
 
-
-    let numberOfIslands = 0;
-
-    for (let row = 0; row < grid.length; row++) {
-        for (let col = 0; col < grid[row].length; col++) {
-            if (grid[row][col] === '1' ) {
-                removeLand(col, row)
-                numberOfIslands++;
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
+            if(grid[i][j] === '1'){
+                removeLand(i, j)
+                numOfIslands++
             }
         }
     }
 
-    return numberOfIslands;
+    return numOfIslands;
 };
