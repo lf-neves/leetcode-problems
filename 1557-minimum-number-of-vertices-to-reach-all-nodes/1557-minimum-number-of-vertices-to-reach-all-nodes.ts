f6bbd09aabd272fe7b5a79e342ab9929bebf9617
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-11-27 09:53:02 -0300

    Time: 608 ms (5.88%), Space: 98.6 MB (5.88%) - LeetHub

---
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.ts
@@ -0,0 +1,19 @@
+function findSmallestSetOfVertices(n: number, edges: number[][]): number[] {
+    const incomeEdgesByNodeMap: Record<number, number> = {}
+
+    for (let i = 0; i < n; i++) {
+        incomeEdgesByNodeMap[i] = 0
+    }
+
+    while (edges.length) {
+        const [fromNode, toNode] = edges.pop()
+
+        console.log(fromNode, toNode)
+
+        incomeEdgesByNodeMap[toNode]++
+    }
+
+
+    return Object.entries(incomeEdgesByNodeMap).filter(([node, incomeEdges]) => incomeEdges === 0).map(([node]) => Number(node))
+
+};
