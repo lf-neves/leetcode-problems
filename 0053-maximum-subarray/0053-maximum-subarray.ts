0196eb8e76772c860bd42e7aa740e4cb2c41a57d
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2025-12-16 10:16:59 -0300

    Time: 1 ms (87.6%), Space: 64.6 MB (62.07%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.ts
@@ -0,0 +1,18 @@
+function maxSubArray(nums: number[]): number {
+    let maxSum = -Infinity
+    let currSum = 0
+
+    for(let i=0;i<nums.length;i++){
+        currSum += nums[i]
+
+        if(maxSum < currSum){
+            maxSum = currSum;
+        }
+
+        if(currSum < 0){
+            currSum = 0
+        }
+    }
+
+    return maxSum;
+};
