f6fe63f68b77fca353725a65cd8b6014deb9d90c
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2026-03-03 09:07:30 -0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.ts
+++ 0015-3sum/0015-3sum.ts
@@ -1,24 +1,36 @@
 function threeSum(nums: number[]): number[][] {
-    nums.sort((a, b) => a - b);
-    const op: number[][] = [];
-    for (let i = 0; i < nums.length; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
-        const target = -nums[i];
-        let left = i + 1, right = nums.length - 1;
-        while (left < right) {
-            const current_sum = nums[left] + nums[right];
-            if (current_sum === target) {
-                op.push([nums[i], nums[left], nums[right]]);
-                while (left < right && nums[left] === nums[left + 1]) left++;
-                while (left < right && nums[right] === nums[right - 1]) right--;
-                left++;
-                right--;
-            } else if (current_sum < target) {
-                left++;
-            } else {
-                right--;
-            }
+    const sortedArr = nums.sort((a,b) => a - b)
+    const result = []
+
+    console.log("Sorted arr: ", sortedArr)
+
+    for (let i = 0; i < sortedArr.length - 2; i++) {
+        let start = i, end = sortedArr.length - 1
+        let partialSum = sortedArr[start] + sortedArr[end]
+
+        console.log(`Checking 2 ----> (${sortedArr[start]})+(${sortedArr[end]})`)
+
+        // we only summed 2 numbers and it's already 0, so the third can't keep it as 0
+        if (partialSum === 0) {
+            continue;
+        }
+
+        let thirdElement;
+
+        if (partialSum > 0) {
+            thirdElement = sortedArr[start + 1]
+        } else {
+            thirdElement = sortedArr[end - 1]
+        }
+
+        console.log(`Checking 3----> (${sortedArr[start]})+(${sortedArr[end]})+(${thirdElement}) = ${partialSum + thirdElement}`)
+
+        if(partialSum + thirdElement === 0){
+            result.push([sortedArr[start], thirdElement, sortedArr[end]])
         }
     }
-    return op;
+
+    console.log(result)
+
+    return result;
 }
