752a1ae16adfbc88065030b30fa61fd66fb0605b
Lucas Neves <84994424+lf-neves@users.noreply.github.com>
2026-03-23 15:15:48 -0300

    Time: 1 ms (86.88%), Space: 56.7 MB (63.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,8 +1,8 @@
 function twoSum(nums: number[], target: number): number[] {
     const complementMap = {}
 
-    for(let i =0;i<nums.length;i++){
-        const complement = target - nums[i];
+    for(let i=0;i<nums.length;i++){
+        const complement = target - nums[i]
 
         if(complementMap[complement] !== undefined){
             return [complementMap[complement], i]
